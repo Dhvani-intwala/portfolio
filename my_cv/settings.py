3dfe0af91ecee06510b938ab88dfa80550e20353
unknown <intdhvani2627@gmail.com>
2024-01-22 13:31:44 +0100

    Create virtual environment

--- my_cv/settings.py
+++ my_cv/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k_rl)h*%$_vbo-hj@)%2$%hj1wzp9-_7+#m_hls0s)%y7vabbq'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
