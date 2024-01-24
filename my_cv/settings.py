392ab5d65943bf6bc6691568544597177c8b6938
unknown <intdhvani2627@gmail.com>
2024-01-24 17:14:24 +0100

    Add a allowed hosts url from heroku

--- my_cv/settings.py
+++ my_cv/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'm-portfolio-889489ce82db.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1' , 'dhvani-intwala-portfolio-7e446227945f.herokuapp.com']
 
 
 # Application definition
