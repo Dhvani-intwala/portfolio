323fb7a3ed8e2c2ebc686578b8cc9e5db867f34c
Dhvani Intwala <intdhvani2627@gmail.com>
2024-03-23 20:05:19 +0100

    Updated media query

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,17 @@
 
 const nav = document.querySelector('#nav');
 
-if(window.innerWidth > 700) {
-window.addEventListener('scroll', () =>{
-    if(window.scrollY >= 56){
-        nav.classList.add('navbar-scrolled');
-    }else if (window.screenY < 56){
-        nav.classList.remove('navbar-scrolled');
-    }
-});
+if (window.innerWidth > 600) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= 56) {
+            nav.classList.add('navbar-scrolled');
+        } else {
+            nav.classList.remove('navbar-scrolled');
+        }
+    });
 }
+
+
 $(document).ready(function() {
     // $('a[href*="#"]').on('click', function(e) {
     //     e.preventDefault();
