b21d11dde29d094ff74f70c93f465973c4ce3514
Dhvani Intwala <intdhvani2627@gmail.com>
2024-03-29 23:16:21 +0100

    Created thank you email body.txt and thank you email subject.txt file

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse, redirect
 from .forms import ContactForm
+from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 
@@ -12,12 +13,15 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
+            subject = form.cleaned_data['subject']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            send_mail(subject, message, email, ['intdhvani2627@gmail.com'])
             form.save()
             messages.success(request, 'Your message has been sent!')
             # return HttpResponseRedirect('/#contact/?submitted=True')
               # Get the current scroll position
             scroll_position = request.POST.get('scroll_position', '0')
-            print(scroll_position)
             # Redirect to the same page with the scroll position as a query parameter
             return redirect('/', scroll_position=scroll_position)
         else:
