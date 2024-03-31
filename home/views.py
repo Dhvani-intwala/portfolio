08ee36eed0b3e8cfe366f6f9e409b6b59875bb57
Dhvani Intwala <intdhvani2627@gmail.com>
2024-03-31 18:34:16 +0200

    updated media query for all devices

--- home/views.py
+++ home/views.py
@@ -13,10 +13,11 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = form.cleaned_data['subject']
+            name = form.cleaned_data['name']
+            phone = form.cleaned_data['phone']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
-            send_mail(subject, message, email, ['intdhvani2627@gmail.com'])
+            send_mail(name, phone, email,  message, ['example@gmail.com'])
             form.save()
             messages.success(request, 'Your message has been sent!')
             # return HttpResponseRedirect('/#contact/?submitted=True')
