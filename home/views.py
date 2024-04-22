76df08266b4436b3e43ef57945f8cd6eafc5aec9
Dhvani Intwala <intdhvani2627@gmail.com>
2024-04-22 14:56:33 +0200

    Add send mail functionality and remove home-flex margin auto

--- home/views.py
+++ home/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.core.mail import send_mail
-from django.http import HttpResponseRedirect
+from django.template.loader import render_to_string
+from django.conf import settings
+# from django.http import HttpResponseRedirect
 from django.contrib import messages
+# from home.models import ContactMessage
 
 # Create your views here.
+def send_confirmation_email(contact_email):
+    """Send the user a confirmation email"""
+    subject = render_to_string(
+        'home/thankyou_emails/thankyou_email_subject.txt',
+    )
+    # Remove line breaks from the subject
+    subject = ''.join(subject.splitlines())
+
+    body = render_to_string(
+        'home/thankyou_emails/thankyou_email_body.txt',
+        {'contact_email': contact_email}
+    )
+
+    send_mail(
+        subject,
+        body,
+        settings.DEFAULT_FROM_EMAIL,
+        [contact_email]
+    )
+
+def send_notification_email(name, email, message):
+    """Send notification email to admin"""
+    subject = 'New Contact Form Submission'
+    body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+    send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [settings.ADMIN_EMAIL])
 
 def contact(request):
     """
@@ -13,20 +41,17 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            phone = form.cleaned_data['phone']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            send_mail(name, phone, email,  message, ['example@gmail.com'])
-            form.save()
+            contact_instance = form.save()
+            send_confirmation_email(contact_instance.email)
+            send_notification_email(contact_instance.name, contact_instance.email, contact_instance.message)
             messages.success(request, 'Your message has been sent!')
+
             # return HttpResponseRedirect('/#contact/?submitted=True')
               # Get the current scroll position
             scroll_position = request.POST.get('scroll_position', '0')
             # Redirect to the same page with the scroll position as a query parameter
             return redirect('/', scroll_position=scroll_position)
         else:
-            form = ContactForm()
             messages.warning(request, 'Message not sent. Please try again.')
 
     else:
@@ -38,4 +63,4 @@ def contact(request):
     context = {
         'form': form,
     }
-    return render(request, 'home/index.html',context)
+    return render(request, 'home/index.html', context)
--- my_cv/settings.py
+++ my_cv/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from django.conf import settings
 from dotenv import load_dotenv
 load_dotenv()
 
+
+ADMIN_EMAIL = settings.EMAIL_HOST_USER
 # if os.path.isfile('env.py'):
 #     import env
 
@@ -164,12 +167,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'intdhvani2627@gmail.com'
+    DEFAULT_FROM_EMAIL = 'intwaladhvani@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_USER = 'intwaladhvani@gmail.com'
+    EMAIL_HOST_PASSWORD = 'nwrqjsmcjwjrudpo'
+    # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+   
+   
